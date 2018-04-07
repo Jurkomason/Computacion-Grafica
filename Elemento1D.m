c6a02b270baf2dc300b87a807807ea98f2222622
jiac <jiac0911@hotmail.com>
2018-04-06 21:30:28 -0500

    estandar

--- Elemento1D.m
+++ Elemento1D.m
@@ -1,6 +1,12 @@
 classdef Elemento1D < figuraGeometrica
     %Vigas y columnas
     properties
+        tipoSeccion=0;
+        base=1;
+        altura=1;
+        puntoa= [0 0 0];
+        puntob=[1 0 0];
+        puntoc=[0 1 0];
     end
     
     methods
