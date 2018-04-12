59fb241aa5939cf2e0d2549b40458bb6abe276f2
Laura <lauris_rp@hotmail.com>
2018-04-11 23:04:37 -0500

    paed

--- main.m
+++ main.m
@@ -243,7 +243,11 @@ function BCrear_Callback(hObject, eventdata, handles)
           e1=(finPared-inicioPared)/norm(finPared-inicioPared);
           p3=[inicioPared(1,1);inicioPared(2,1)+3;inicioPared(3,1)];
           p1p3=(p3-inicioPared);
-          e2=(p1p3-(p1p3.'*e1)*e1)/norm(p1p3-(p1p3.'*e1)*e1);
+          if e1(1,1)==0 && e1(3,1)==0
+              e2=[-1;0;0];
+          else
+              e2=(p1p3-(p1p3.'*e1)*e1)/norm(p1p3-(p1p3.'*e1)*e1);
+          end 
           e2=e2/norm(e2);
           e3=cross(e1,e2);
           
